bef422b834a467333ec1b6b4025342b13909fa89
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-04-01 23:44:46 -0300

    better docs and util module created

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,16 +6,16 @@ rm -rf lib/src/dist;
 
 echo "----------------------------------------";
 echo "Installing bower components...";
-./node_modules/bower/bin/bower install --config.interactive=false -F
+./node_modules/bower/bin/bower install --config.interactive=false -F;
 
 echo "----------------------------------------";
 echo "Building files using Grunt...";
-./node_modules/grunt-cli/bin/grunt default
+./node_modules/grunt-cli/bin/grunt default;
 
 echo "----------------------------------------";
 echo "Generating docs using JSDoc...";
-
-./node_modules/jsdoc/jsdoc.js -c conf.json
+rm -rf lib/docs;
+./node_modules/jsdoc/jsdoc.js -c conf.json;
 
 echo "----------------------------------------";
 echo "Post install script OK!";
