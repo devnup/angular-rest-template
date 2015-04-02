14ccdfc979eaf547d875f66b69e073bcf9d6ee0e
Luis Eduardo Brito <luiseduardo14@gmail.com>
2015-04-01 22:12:37 -0300

    more detailed build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,86 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        copy: {
-            main: {
-                src: 'lib/src/angular-rest-template.js',
-                dest: 'lib/dist/js/angular-rest-template.js'
-            }
-        },
+    concat: {
+      simple: {
+        src: [
+          'lib/src/**.js'
+        ],
+        dest: 'lib/dist/js/angular-rest-template.js'
+      },
+      withAsync: {
+        src: [
+          'lib/bower_components/async/lib/async.js',
+          'lib/src/**.js'
+        ],
+        dest: 'lib/dist/js/angular-rest-template-with-async.js'
+      },
+      withNgStorage: {
+        src: [
+          'lib/bower_components/ngstorage/ngstorage.js',
+          'lib/src/**.js'
+        ],
+        dest: 'lib/dist/js/angular-rest-template-with-ng-storage.js'
+      },
+      full: {
+        src: [
+          'lib/bower_components/async/lib/async.js',
+          'lib/bower_components/ngstorage/ngstorage.js',
+          'lib/src/**.js'
+        ],
+        dest: 'lib/dist/js/angular-rest-template-full.js'
+      }
+    },
 
-        uglify: {
-            minify: {
-                files: {
-                    'lib/dist/js/angular-rest-template.min.js': ['lib/dist/js/angular-rest-template.js']
-                }
-            }
-        },
-
-        less: {
-            development: {
-                options: {
-                    paths: ['lib/assets/less']
-                },
-                files: {
-                    'lib/assets/css/style.css': 'lib/assets/less/**.less'
-                }
-            }
-        },
-
-        cssmin: {
-            target: {
-                files: {
-                    'lib/assets/css/style.min.css': ['lib/dist/css/style.css']
-                }
-            }
-        },
-
-        clean: ["lib/dist/", "lib/assets/css/"],
-
-        watch: {
-            files: ['lib/**'],
-            tasks: ['dev']
+    uglify: {
+      minify: {
+        files: {
+          'lib/dist/js/angular-rest-template.min.js': ['lib/dist/js/angular-rest-template.js'],
+          'lib/dist/js/angular-rest-template-with-async.min.js': ['lib/dist/js/angular-rest-template-with-async.js'],
+          'lib/dist/js/angular-rest-template-with-ng-storage.min.js': ['lib/dist/js/angular-rest-template-with-ng-storage.js'],
+          'lib/dist/js/angular-rest-template-full.min.js': ['lib/dist/js/angular-rest-template-full.js']
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    less: {
+      development: {
+        options: {
+          paths: ['lib/assets/less']
+        },
+        files: {
+          'lib/assets/css/style.css': 'lib/assets/less/**.less'
+        }
+      }
+    },
 
-    grunt.registerTask('dev', ['clean', 'copy', 'uglify', 'less', 'cssmin']);
-    grunt.registerTask('default', ['dev']);
+    cssmin: {
+      target: {
+        files: {
+          'lib/assets/css/style.min.css': ['lib/dist/css/style.css']
+        }
+      }
+    },
+
+    clean: ["lib/dist/", "lib/assets/css/"],
+
+    watch: {
+      files: ['lib/**'],
+      tasks: ['dev']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('dev', ['clean', 'concat', 'uglify', 'less', 'cssmin']);
+  grunt.registerTask('default', ['dev']);
 };
